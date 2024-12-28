6da2ccc5b0cb51fb813b547c4c70e7953f72ca16
R1013-T <takahashi.ryunosuke.job@gmail.com>
2024-12-28 19:27:37 +0900

    Initial Commit

---
+++ swift-tutorial-chat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-tutorial-chat
+//
+//  Created by 髙橋龍之介 on 2024/12/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-tutorial-chat/swift_tutorial_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_tutorial_chatApp.swift
+//  swift-tutorial-chat
+//
+//  Created by 髙橋龍之介 on 2024/12/28.
+//
+
+import SwiftUI
+
+@main
+struct swift_tutorial_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
